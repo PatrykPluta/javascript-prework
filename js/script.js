3f372d7bd1d130028ded2c1e9bae28fa25d794dd
Patryk Pluta <patryk_pluta@interia.pl>
2021-12-15 18:30:41 +0100

    losowanie

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2){
+  computerMove = 'papier';
+} else {(randomNumber == 3)
+  computerMove = 'nożyce';
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
-
-let computerMove = `kamień`;
-
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-
-let playerMove = `papier`;
-
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+printMessage('Mój ruch to: ' + computerMove);
